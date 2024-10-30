6ba1320629b5fc7ba6a14a154dcf52ed0a481be0
Anthony Wangala <143312421+Brain-Wave254@users.noreply.github.com>
2024-10-30 09:04:16 +0300

    Initial commit

---
+++ Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManagerX : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI timeText;
+    public GameObject titleScreen;
+    public Button restartButton;
+
+    public List<GameObject> targetPrefabs;
+
+    private int score;
+    private float spawnRate = 1.5f;
+    public bool isGameActive;
+
+    private float timeLeft = 60f;
+
+    private float spaceBetweenSquares = 2.5f;
+    private float minValueX = -3.75f;
+    private float minValueY = -3.75f;
+
+    public void StartGame()
+    {
+        titleScreen.SetActive(false);
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        StartCoroutine(CountdownTimer());
+    }
+
+    IEnumerator CountdownTimer()
+    {
+        while (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timeText.text = "Time: " + Mathf.Round(timeLeft).ToString();
+            yield return null;
+        }
+        GameOver();
+    }
+
+    public void SetDifficulty(int difficulty)
+    {
+        switch (difficulty)
+        {
+            case 1:
+                spawnRate = 1.5f;
+                break;
+            case 2: 
+                spawnRate = 1.0f;
+                break;
+            case 3: 
+                spawnRate = 0.5f;
+                break;
+        }
+
+        titleScreen.SetActive(false);
+        StartGame();
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+
+            if (isGameActive)
+            {
+                Instantiate(targetPrefabs[index], RandomSpawnPosition(), targetPrefabs[index].transform.rotation);
+            }
+        }
+    }
+
+    Vector3 RandomSpawnPosition()
+    {
+        float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
+        float spawnPosY = minValueY + (RandomSquareIndex() * spaceBetweenSquares);
+
+        Vector3 spawnPosition = new Vector3(spawnPosX, spawnPosY, 0);
+        return spawnPosition;
+    }
+
+    int RandomSquareIndex()
+    {
+        return Random.Range(0, 4);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
